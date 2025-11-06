bc392131cf3213b18dac6429be41894586c208bf
Ryan Fonseka <ryanfonseka@gmail.com>
2025-11-06 10:10:15 +0530

    logs removed

--- sql-connection.js
+++ sql-connection.js
@@ -2,9 +2,6 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-console.log("pass",process.env.DB_USER, process.env.DB_PASSWORD);
-
-
 const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
