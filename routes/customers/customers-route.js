697a82903832bd7bcfeca715d8f69c52da502dd0
Ryan Fonseka <ryanfonseka@gmail.com>
2025-12-03 11:38:17 +0530

    Customer API added

--- routes/customers/customers-route.js
+++ routes/customers/customers-route.js
@@ -2,6 +2,13 @@ const express = require("express");
 const customerController = require("../../controllers/customers/customer-controller");
 const customerRouter = express.Router();
 
-customerRouter.route("/").get(customerController.getAllCustomers);
+customerRouter.route("/")
+    .get(customerController.getAllCustomers)
+    .post(customerController.createCustomer);
+customerRouter
+  .route("/:customerId")
+  .get(customerController.getCustomerById)
+  .put(customerController.updateCustomer)
+  .delete(customerController.deleteCustomer);
 
 module.exports = customerRouter;
