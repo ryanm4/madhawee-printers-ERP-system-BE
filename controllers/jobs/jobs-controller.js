9d36db2b472b5fc336dcfcba8eed5eb86a3634e0
IndulaH <anupaindula50@gmail.com>
2026-09-01 07:29:55 +0530

    changes

--- controllers/jobs/jobs-controller.js
+++ controllers/jobs/jobs-controller.js
@@ -3,7 +3,7 @@ const pool = require("../../sql-connection");
 const JOB_NUMBER_TEMPLATES = {
   TIEP: "MPL/####/YY/TIEP",
   "NON_TIEP": "MPL/####/YY/NON_TIEP",
-  MP: "MPL/####/YY/PATHUM",
+  MP: "MP/####/YY/PATHUM",
 };
 
 // FIX 1: Robustly handle undefined/null/invalid date values
